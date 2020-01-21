a5ef32ac1b3066fd1301680ba7666a322ed0f21e
Floch <gaelle_floch@hotmail.com>
2020-01-21 09:50:51 +0100

    correction erreur implement method project fixtures

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -53,4 +53,16 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
 
         $manager->flush();
     }
+
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return class-string[]
+     */
+    public function getDependencies()
+    {
+        return [SkillFixtures::class];
+    }
 }
