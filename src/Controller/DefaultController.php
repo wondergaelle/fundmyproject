e47da3c3e3c225b8de989966f5d34c285fd2ebbb
Floch <gaelle_floch@hotmail.com>
2020-01-22 10:05:57 +0100

    affichage dynamique des projets

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,17 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        // rechercher les données dans la base de données
+        $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
+        $categories =$this->getDoctrine()->getRepository(Category::class)->findAll();
+
+
+
+
+        //envoyer les données dans la vue
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'projects'=>$projects,
+            'categories'=>$categories,
         ]);
     }
 }
